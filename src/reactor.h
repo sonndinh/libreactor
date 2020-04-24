5f2622c1a6547ed1bd6783a957975a4f09de122c
Son Dinh <dinhs@objectcomputing.com>
2020-04-24 00:31:18 -0500

    refactoring

--- src/reactor.h
+++ src/reactor.h
@@ -1,4 +1,4 @@
-/*
+/**
  * =====================================================================================
  *  FILENAME  :  Reactor.h
  *  DESCRIPTION :  Reactor class uses Singleton and Bridge pattern to
@@ -28,14 +28,6 @@
 
 class ReactorImpl;
 
-
-/*
- * =====================================================================================
- *        Class:  Reactor
- *  Description:  Reactor is in charge of demultiplexing and dispatching events
- * =====================================================================================
- */
-
 /**
  * @class Reactor
  *
@@ -43,91 +35,54 @@ class ReactorImpl;
  */
 class Reactor {
 protected:
-  /* 
-   * ===  FUNCTION  ======================================================================
-   *         Name:  Reactor()
-   *  Description:  Construtor is protected so it assures that there is only 
-   *          a instance of Reactor ever created.
-   * =====================================================================================
-   */
   Reactor();
   ~Reactor();
 
 public:
-    
-  /* 
-   * ===  FUNCTION  ======================================================================
-   *         Name:  mRegisterTCPCbFuncs()
-   *  Description:  Function to register user callbacks for incoming TCP data 
-   *          and TCP events.
-   * =====================================================================================
-   */
-  virtual void register_tcp_callbacks(ReactorStreamHandleRead read_cb, ReactorStreamHandleEvent event_cb){
+  /**
+   * @brief Register callbacks for TCP events.
+   */    
+  virtual void register_tcp_callbacks(ReactorStreamHandleRead read_cb,
+                                      ReactorStreamHandleEvent event_cb) {
     tcp_read_handler_ = read_cb;
     tcp_event_handler_ = event_cb;
   }
 
-  /* 
-   * ===  FUNCTION  ======================================================================
-   *         Name:  mRegisterUDPCbFuncs()
-   *  Description:  Function to register user callbacks for incoming UDP datagrams
-   *          and UDP events.
-   * =====================================================================================
+  /**
+   * @brief Register callbacks for UDP events.
    */
-  virtual void register_udp_callbacks(ReactorDgramHandleRead read_cb, ReactorDgramHandleEvent event_cb){
+  virtual void register_udp_callbacks(ReactorDgramHandleRead read_cb,
+                                      ReactorDgramHandleEvent event_cb) {
     udp_read_handler_ = read_cb;
     udp_event_handler_ = event_cb;
   }
 
-  /* 
-   * ===  FUNCTION  ======================================================================
-   *         Name:  mRegisterHandler()
-   *  Description:  Interface for registering handler
-   * =====================================================================================
-   */
   virtual void register_handler(EventHandler* eh, EventType et);
   virtual void register_handler(Socket h, EventHandler* eh, EventType et);
 
-  /* 
-   * ===  FUNCTION  ======================================================================
-   *         Name:  mRemoveHandler()
-   *  Description:  Interface for removing handler
-   * =====================================================================================
-   */
   virtual void remove_handler(EventHandler* eh, EventType et);
   virtual void remove_handler(Socket h, EventType et);
 
   ReactorImpl* get_reactor_impl();
   
   /* 
-   * ===  FUNCTION  ======================================================================
-   *         Name:  mHandleEvents()
-   *  Description:  Main loop for handling incoming events
-   * =====================================================================================
+   * @brief Main loop for handling incoming events.
    */
   void handle_events(TimeValue* timeout=nullptr);
   
-  /* 
-   * ===  FUNCTION  ======================================================================
-   *         Name:  sInstance()
-   *  Description:  Access point for user to get solely instance of this class
-   * =====================================================================================
-   */
   static Reactor* instance(DemuxType type=SELECT_DEMUX);
-  
-  
+
 public:
   ReactorStreamHandleRead   tcp_read_handler_;
   ReactorStreamHandleEvent  tcp_event_handler_;
   ReactorDgramHandleRead    udp_read_handler_;
   ReactorDgramHandleEvent   udp_event_handler_;
-  
+
 protected:
-  //Pointer to abstract implementation of Reactor
-  //using to Bridge pattern
+  /// Implementation of Reactor using Bridge pattern.
   static ReactorImpl* reactor_impl_;
 
-  //Pointer to a process-wide Reactor singleton
+  /// Process-wide Reactor singleton.
   static Reactor* reactor_;
 };
 
